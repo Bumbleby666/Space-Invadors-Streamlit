f95309bacc7d3ecab32616893ad2fd2b5f53ac29
Bumbleby666 <124770328+Bumbleby666@users.noreply.github.com>
2023-02-13 13:50:28 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+
+
+st.title("Hallo, erste Versuche mit Space Invadors!")
+#----Header Selection-----
+with st.container():
+    st.subheader("Hallo, wir sind die Gruppe Dehli !!")
+    st.title("")
